be9deb42e67d02362160ad83e1ee9de4a8edf65c
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2023-12-29 09:08:27 +0700

    01. Starter Project

---
+++ src/test/java/com/iffat/spring6/dependencyinjection/Spring6DependencyInjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iffat.spring6.dependencyinjection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6DependencyInjectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
