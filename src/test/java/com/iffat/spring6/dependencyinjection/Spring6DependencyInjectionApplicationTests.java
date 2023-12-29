d9215be2f9a3fadf085303475eebc017ebcfd77a
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2023-12-29 09:29:36 +0700

    03. Spring test context

--- src/test/java/com/iffat/spring6/dependencyinjection/Spring6DependencyInjectionApplicationTests.java
+++ src/test/java/com/iffat/spring6/dependencyinjection/Spring6DependencyInjectionApplicationTests.java
@@ -1,10 +1,30 @@
 package com.iffat.spring6.dependencyinjection;
 
+import com.iffat.spring6.dependencyinjection.controllers.MyController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class Spring6DependencyInjectionApplicationTests {
+
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @Autowired
+    MyController myController;
+
+    @Test
+    void testAutowireOfController() {
+        System.out.println(myController.sayHello());
+    }
+
+    @Test
+    void testGetControllerFromContext() {
+        MyController myController = applicationContext.getBean(MyController.class);
+        System.out.println(myController.sayHello());
+    }
 
     @Test
     void contextLoads() {
